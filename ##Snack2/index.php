399f6830552a41d507b51f257ca47fb1f83a0b9e
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-05 17:56:01 +0200

    .

--- ##Snack2/index.php
+++ ##Snack2/index.php
@@ -1,5 +1,14 @@
 <?php
 
+
+/*
+## Snack 2
+Passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”,
+ altrimenti “Accesso negato”
+*/
+
 $name = isset($_GET['name'])? $_GET['name'] : '';
 $mail = isset($_GET['email'])? $_GET['email'] : '';
 $age = isset($_GET['age'])? $_GET['age'] : '';
--- ##Snack4/index.php
+++ ##Snack4/index.php
@@ -1,6 +1,10 @@
 <?php
 
-
+/*
+## Snack 4
+Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+*/
 
 //creo funzione che crea numeri randomici fino ad un max dinumeri pescati
 function rundomNumbers($min, $max, $arrayLength){
